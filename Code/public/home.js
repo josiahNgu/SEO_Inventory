0055156ebbe11a9b293105573b0d457045c62bd5
Josiah Ngu <37003609+josiahNgu@users.noreply.github.com>
2018-03-29 08:50:33 -0500

    Add files via upload

---
+++ Code/public/home.js
@@ -0,0 +1,91 @@
+//add Item modal
+var modal = document.getElementById('simpleModal');
+var modalBtn = document.getElementById('addItem');
+var closeBtn = document.getElementsByClassName('closeBtn')[0];
+
+modalBtn.addEventListener('click',openModal);
+closeBtn.addEventListener('click',closeModal);
+
+
+//add category Modal
+var categoryModal = document.getElementById('categoryModal');
+var categoryB = document.getElementById('addCategory');
+var closeCategory = document.getElementsByClassName('closeBtn')[0];
+
+categoryB.addEventListener('click',openCatgy);
+closeCategory.addEventListener('click',closeCategory);
+// var catModal = document.getElementById('categoryModal');
+// var catBtn = document.getElementById('addCategory');
+// var closecat = document.getElementsByClassName('closeBtn')[0];
+
+// catBtn.addEventListener('click',openCatgy);
+// closecat.addEventListener('click',closeCatgy);
+
+//edit item modal
+var editItem = document.getElementById('editItemModal');
+var editBtn = document.getElementById('editItem');
+var closeEdit = document.getElementsByClassName('editItemClose')[0];
+
+editBtn.addEventListener('click',openEditItem);
+closeEdit.addEventListener('click',closeEditItem);
+
+
+//edit category modal
+var editCatgy = document.getElementById('editCategoryMdl');
+var editCatgyBtn = document.getElementById('editCategoryN');
+var closeEditCatgy = document.getElementsByClassName('EcatClose')[0];
+
+editCatgyBtn.addEventListener('click',openEditCatgy);
+closeEditCatgy.addEventListener('click',closeEditCatgy);
+
+window.addEventListener('click', outsideClick);
+//fuction to  addItem modal
+function openModal(){
+     modal.style.display = 'block';
+}
+
+function closeModal(){
+    modal.style.display = 'none';
+}
+
+function outsideClick(e){
+    if(e.target == modal){
+    modal.style.display ='none';
+    }
+    if(e.target == categoryModal){
+        categoryModal.style.display = "none";
+    }
+    if(e.target == editCatgy){
+        editCatgy.style.display = 'none';
+    }
+    if(e.target == editItem){
+        editItem.style.display ='none';
+    }
+}
+
+//function to category modal
+function openCatgy(){
+    categoryModal.style.display = "block";
+}
+
+function closeCatgy(){
+    categoryModal.style.display = "none";
+}
+
+//function to edit category modal
+function openEditCatgy(){
+    editCatgy.style.display = 'block';
+}
+
+function closeEditCatgy(){
+    editCatgy.style.display = 'none';
+}
+
+//function to edit item
+function openEditItem(){
+    editItem.style.display = 'block';
+}
+
+function closeEditItem(){
+    editItem.style.display = 'none';
+}
